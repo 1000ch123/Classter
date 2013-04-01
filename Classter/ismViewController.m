6f945acd8c612329ac4085ec3b0111dde02e0b9d
kanade <kanade@kanade-no-MacBook-Pro.local>
2013-04-01 15:11:47 +0900

    Initial Commit

---
+++ Classter/ismViewController.m
@@ -0,0 +1,29 @@
+//
+//  ismViewController.m
+//  Classter
+//
+//  Created by kanade on 13/04/01.
+//  Copyright (c) 2013年 kanade. All rights reserved.
+//
+
+#import "ismViewController.h"
+
+@interface ismViewController ()
+
+@end
+
+@implementation ismViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
